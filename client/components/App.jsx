323ad6fd5e746991402634bc87a6743cf7b1408c
petemcnie <petemcnie0@gmail.com>
2020-08-31 15:53:23 +1200

    Navbar component created

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
-// import { Route } from 'react-router-dom'
+import { Route } from 'react-router-dom'
+
+import NavbarComp from './NavbarComp'
 
 const App = () => {
   return (
     <>
+    <Route path='/' component={NavbarComp} />
       <h1>hello world</h1>
     </>
   )
---
+++ client/components/NavbarComp.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Navbar from 'react-bootstrap/Navbar'
+
+const NavbarComp = () => {
+    return (
+        <div>
+            <h3>navbar</h3>
+        </div>
+    )
+}
+
+export default NavbarComp
