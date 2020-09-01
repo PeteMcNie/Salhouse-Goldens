a6904f6c012074e1725b8adecad7e4b1d7f97793
petemcnie <petemcnie0@gmail.com>
2020-09-01 17:10:44 +1200

    Text added to HomeBody

--- client/components/HomeBody.jsx
+++ client/components/HomeBody.jsx
@@ -2,7 +2,26 @@ import React from 'react'
 
 function HomeBody () {
   return (
-    <h4>Lots of text here...</h4>
+    <>
+      <div>
+        <h1>Welcome to Salhouse Golden Retrievers</h1>
+      </div>
+      <div>
+        <p>
+            I am a dog lover who fell in love with this beautiful breed when I was a child in England.
+            If you would like to read the story of the animals in my life please go to the My Life with Animals page.
+        </p>
+        <p>
+            The golden retrievers in the above photograph are Tuppence, Shadow, Kula, Sherry and Autumn, with Phoenix laying
+            down in front. Unfortunately our dear sweet Phoenix passed over the Rainbow Bridge in September 2016 at the ripe
+            old age of 14 years, 8 months. However, she is still very much in our hearts and minds and it is very hard to think
+            about the fact that she is no longer with us. My website will still tell the story of Phoenix because she was the
+            most beautiful first golden for me to own and was the reason I wanted to have more of them. I can't thank her enough
+            for the lessons she taught me and the loyalty only a dog can give even when the human is less than perfect.
+        </p>
+      </div>
+
+    </>
   )
 }
 
--- server/public/bundle.js
+++ server/public/bundle.js
@@ -209,7 +209,7 @@ __webpack_require__.r(__webpack_exports__);
 
 
 function HomeBody() {
-  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h4", null, "Lots of text here...");
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react__WEBPACK_IMPORTED_MODULE_0___default.a.Fragment, null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", null, "Welcome to Salhouse Golden Retrievers")), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, "I am a dog lover who fell in love with this beautiful breed when I was a child in England. If you would like to read the story of the animals in my life please go to the My Life with Animals page."), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, "The golden retrievers in the above photograph are Tuppence, Shadow, Kula, Sherry and Autumn, with Phoenix laying down in front. Unfortunately our dear sweet Phoenix passed over the Rainbow Bridge in September 2016 at the ripe old age of 14 years, 8 months. However, she is still very much in our hearts and minds and it is very hard to think about the fact that she is no longer with us. My website will still tell the story of Phoenix because she was the most beautiful first golden for me to own and was the reason I wanted to have more of them. I can't thank her enough for the lessons she taught me and the loyalty only a dog can give even when the human is less than perfect.")));
 }
 
 /* harmony default export */ __webpack_exports__["default"] = (HomeBody);
