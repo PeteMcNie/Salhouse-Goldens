6d988a21b959f0eb57d3abd6e7b3d664c19b0889
petemcnie <petemcnie0@gmail.com>
2020-09-01 19:14:25 +1200

    Snapshot tests updated

--- client/components/HomeBody.test.jsx
+++ client/components/HomeBody.test.jsx
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react'
 
 import HomeBody from './Home'
 
-test('Home component to match snapshot', () => {
+test('HomeBody component to match snapshot', () => {
   const { container } = render(<HomeBody />)
   expect(container).toMatchSnapshot()
 })
