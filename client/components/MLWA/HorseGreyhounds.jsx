e5c31e7be8e9c5566cefdcd1bd8c908e08a909df
petemcnie <petemcnie0@gmail.com>
2020-09-02 19:30:55 +1200

    HorseGreyhound component created

---
+++ client/components/MLWA/HorseGreyhounds.jsx
@@ -0,0 +1,90 @@
+import React from 'react'
+
+import Image from 'react-bootstrap/Image'
+
+function HorseGreyhounds () {
+  return (
+    <>
+      <div>
+        <h3>Horses</h3>
+        <p>
+            In the meantime, when I wasn&apos;t walking Lassie, I was always up the local stables riding the horses, mucking out
+            and as I got older, teaching other people to ride. I broke my arm while riding on the South Downs one day. We were
+            in a field near Cissbury Ring - the furthest away from the stables that this particular 1 hour ride would go, on a
+            Saturday afternoon. While we were walking through the field some idiots let off a gun to shoot at the pheasants.
+            Well, the whole ride took off at a flat out gallop. I was only 9 years old at the time and I was riding the fastest
+            pony that we had. His name was Dr Zhivago and he was very strong. The horses were all galloping on the very hard
+            ground as it was middle of summer. The Downs are made of chalk and it is not very pleasant to land on. Zhivago was
+            going flat out and I dropped one rein and pulled his head round and even put my fingers through the ring of the bit
+            but I couldn&apos;t slow him down. We were coming to a very sharp bend in the track and at the speed we were going I
+            thought he was going to go down and probably fall on top of me and so I made the decision to get off at full speed
+            gallop. Of course, in hindsight I should perhaps have stayed with him, but then worse could have happened if he did
+            fall at the corner. I took my feet out of the stirrups as I didn&apos;t want to be caught up in them and then got off -
+            no easy feat when the horse is going so fast. I hit the ground and I remember putting my head up and hearing the
+            thunder of feet, so I put my head down again while another horse galloped on past me (remember that there were
+            probably 20 horses bolting as fast as they could at this stage). Then everything went quiet and I waited. I got up
+            and suddenly realised that my watch on my left arm was in the middle of a very big dip in my wrist. I had broken it.
+            I also had a small stone lodged in the corner of one of my eyes, probably when my head hit the ground. I started to
+            walk back to the stables with one of the other ladies who had stopped her horse and was picked up by a farmer part
+            way down the track and bounced very uncomfortably down the track to his house where he called my parents. Apart from
+            that one accident, working with horses was great fun and allowed me to be around animals even more which was all I
+            wanted.
+        </p>
+        <Image src="images/topper.jpg" fluid />
+        <hr />
+      </div>
+      <div>
+        <h3>Greyhounds</h3>
+        <p>
+            Once I finished school I started work at a racing greyhound kennel in the Sussex countryside. My Dad got me the job,
+            so although he hadn&apos;t seemed to actively promote my love of animals, he was responsible for helping me start my
+            working life with them. This was probably more because he was a betting man and loved to go to horse racing at
+            Fontwell Park and the greyhound racing at Brighton. This gave him an excuse to go to lots of greyhound meetings
+            because he would have to come and pick me up at the kennels after a race night, so he and Mum used to come to the
+            meetings when I was working. Not only that, but he became friendly with the trainer I worked with, George Curtis,
+            and George would give him a tip for a bet each night.
+        </p>
+        <p>
+            I used to work one or two nights a week. On work days I would start out from home at 6am and walk to the station,
+            catch the train to Portslade, wait on the corner of the road for a van to come and pick me up along with all the
+            other kennel hands and then drive to the kennels for work at around 8am. On a day when I was working the evening
+            race meeting, we would stay at the kennels after 4pm which was our usual finish time and wait until it was time to
+            load up all those dogs that were going to be racing and then get in the back of the lorry with the dogs and travel
+            to the stadium. We would then have to unload all the dogs, walk them round to get a urine test, weigh them in and
+            put them in their kennel. Then we would have a meal and then start the evening&apos;s racing. We would have to get each
+            dog out of the kennel for its race, have it identified, put a jacket on it and then see it off to the race. Some
+            nights I would work as a walker, walking the dogs on the track in front of the crowd and then putting my runner
+            in the trap ready for the race. Then we would have to run to the other side of the track where the finish was and
+            catch our dog. On days where I worked the evening race meeting, we wouldn&apos;t finish until about 11.30pm and by the
+            time we drove home it would be past midnight. Quite hard if I was working again the next day and had to be up
+            before 6am.
+        </p>
+        <div>
+          <Image src="images/clountieComment.jpg" fluid />
+          <p>Pictured left is me with one of the greyhounds, a female whose racing name was Clountie Comment. I can&apos;t recall
+              her kennel name.</p>
+        </div>
+        <p>
+            Those were good, fun days. I learned to love greyhounds as much as any other dog. They don’t look that cuddly, but
+            in fact they can be. I used to sit in the kennels with them sometimes when we were waiting for something to happen.
+            They all had their own personalities and most loved contact with people. They make lovely pets. I had a particular
+            favourite called Deneholme Deluxe. His kennel name was Sam. He was a cheeky dog and obviously just to my liking as
+            I had a special bond with him. However, he wasn’t the best racing dog and his owners knew I liked him and I can recall
+            some suggestion that I might like him as a pet. But I couldn’t do that as my Dad wouldn’t let me so he was sent to
+            Portsmouth to run there. I don’t know what happened to him in the end. I hope that he finished his racing career and
+            found someone to give him a lovely home for the rest of his life.
+        </p>
+        <p>
+            I had a couple of stints of working with the greyhounds, but eventually I had to give it up as I got such bad hayfever
+            that my doctor warned that I might start to get asthma if I continued working with them. The kennels were in the middle
+            of the Sussex countryside and in June and July I could hardly breathe every day. I would sneeze my way through the day
+            and twice my Dad had to come and pick me up and take me home because I had collapsed. So I gave up the dogs and started
+            the boring and unsatisfying career of working in offices.
+        </p>
+        <hr />
+      </div>
+    </>
+  )
+}
+
+export default HorseGreyhounds
