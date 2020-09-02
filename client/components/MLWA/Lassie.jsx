e2edc3a1a1b5dbac34f02b5471d518ae21be124e
petemcnie <petemcnie0@gmail.com>
2020-09-02 15:43:46 +1200

    Lassie content added to component

---
+++ client/components/MLWA/Lassie.jsx
@@ -0,0 +1,80 @@
+import React from 'react'
+
+import Image from 'react-bootstrap/Image'
+
+function Lassie () {
+  return (
+    <div>
+      <h3>Lassie</h3>
+      <p>
+            What I really wanted was a dog, but my Dad was never going to allow that. I knew that there was an elderly couple
+            across the road from us who owned a beautiful dog. I had seen them walking her up the road. She was only young and
+            I was five years old and I decided to ask them if I could take their dog for a walk. My heart was in my mouth as I
+            walked up to their front door and knocked on it, feeling so small and not knowing what they were going to say. I
+            couldn’t believe my luck when they said they were happy for me to take her for a walk. Her name was Lassie, and this
+            was the start of a beautiful friendship and the beginning of my love of Golden Retrievers. Below is the only photo I
+            have of Lassie.
+      </p>
+      <Image src="images/lassie.jpg" fluid />
+      <p>
+            Lassie was not actually a purebred Golden Retriever, but a Saluki/Golden Retriever cross. She had the build of a Golden
+            and the beautiful golden colouring, but her feathers were very long, more like a Saluki. I have to give thanks to Claude
+            and Dorothy (Lassie’s owners) for allowing me the chance to walk their dog – I know that if a 5-year old child came to
+            me to ask if they could walk one of my dogs I would say no.
+      </p>
+      <p>
+            As I grew up I did everything with Lassie. I walked her after school every day, took her out for whole day walks at the
+            weekend, sometimes to the beach, sometimes up the South Downs. We would get lost and walk miles but eventually find our
+            way home. Claude and Dorothy had a grand-daughter, Alison, who used to come and visit with her parents about one Sunday
+            every month. I used to sit at the lounge window and hope every Sunday that this would be the day they arrived, and when
+            they did, Alison and I went for hikes even further. Alison was more of a tom boy and daredevil than I was and I enjoyed
+            her company. She had two Jack Russells and so we would go walking with the three dogs. I used to love having Lassie or one
+            of the Jack Russells on a lead walking round the streets. I can’t explain the feeling, it was like being privileged to
+            have this dog trotting along with me. When we got to the beach, park or Downs the dogs would be let off the lead and we
+            would go wandering, in the bush, sometimes through a field that we knew we shouldn’t be going through with a dog – what if
+            the farmer saw us? We used to walk in the snow as well in winter. I remember that Lassie had a lot of fur between her toes
+            and under her paws. When we walked up the Downs in the snow, ice balls would form in the fur under her paws and I would
+            have to stop every so often to break the ice up and pull it off the fur. Then we would carry on for a bit longer until I
+            had to do it again. We went far and wide and had some great adventures.
+      </p>
+      <p>
+            I think one of the greatest thing about dogs is the fact that they are so happy to see you. Every day that I went to Claude
+            and Dorothy’s to take Lassie out, she would go wild with excitement seeing me. Claude used to take Lassie out a couple of
+            times a day, but these were more sedate walks and I like to think that she enjoyed the adventure that she would have when
+            we went out together.
+      </p>
+      <p>
+            Sometimes I used to take her into my home before or after a walk. My Dad was at work every day and my Mum didn’t mind Lassie
+            being in the house for a while. I know that there were occasions when Lassie was in the house when Dad was there, so I think
+            he realised that I had a friend that could go back home at the end of the day but that he was prepared to tolerate in small
+            doses. I do remember that he never patted or greeted Lassie. He acknowledged that she was part of my life, but (although he
+            didn’t dislike animals) he just wasn’t affectionate over them. One day Lassie was round our house and Dad was home. Dad and
+            I were playing and we were standing facing each other and I had my hands out and Dad put his hands either side of mine and
+            clapped my hands together. We did that a few times and it is the only time that I saw Lassie try to protect me. She obviously
+            thought my Dad was attacking me because she jumped up and tried to separate us.
+      </p>
+      <p>
+            I can remember grooming Lassie. I didn’t have any equipment at all, but every so often I remember that she had all this fluffy
+            fur that would start to come out in clumps. So I would take her into the garden, lay her down and then “pluck” the fur out with
+            my fingers. I used to love the transformation from a slightly paler looking fluffy dog to a tidier, darker golden coat again.
+            Lassie was very patient and didn’t seem to mind me doing this. She was the most beautiful dog and I will never forget her.
+      </p>
+      <p>
+            We grew up together, Lassie and I. As she got older I sometimes only went and sat with Claude and Dorothy for a chat and
+            didn’t walk Lassie. Sometimes I would take her just up the road and back as she began to have problems with her legs. To be
+            quite honest, although I knew she was old, I don’t think I ever thought about her dying. One day I went across the road and
+            knocked on the door as usual and Claude came to the door. One look at him and I knew something was wrong. Lassie had been
+            put to sleep earlier that day. I remember my Mum and Dad were out that evening and I didn’t want to go back to an empty
+            house, so I went to another neighbour that I knew well and stayed with them for the evening until my Mum and Dad came home.
+      </p>
+      <p>
+            The one thing I regret is that I was never given the chance to say goodbye to Lassie. I would have liked one last cuddle
+            and one last promise to love her forever and one last kiss on that beautiful head of hers. However, I am sure she knows
+            that. She gave me the greatest gift of all, the love of a dog – thank you Lassie.
+      </p>
+      <hr />
+    </div>
+  )
+}
+
+export default Lassie
--- client/components/MLWA/MyLifeWithAnimals.jsx
+++ client/components/MLWA/MyLifeWithAnimals.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 
+import NavbarComp from '../NavbarComp'
+import Lassie from './Lassie'
+
 function MyLifeWithAnimals () {
   return (
-    <div>
-      <p>MLWA</p>
-    </div>
+    <>
+      <NavbarComp />
+      <div className='belowNavbar'>
+        <h2>My Life With Animals</h2>
+        <p>
+            My love of animals started when I was growing up in England. My Dad was not really receptive to having animals
+            in the house. However, I presume that at some stage my Mum persuaded him that it would be good for me to have a
+            furry creature as I remember I had a hamster called Jingle Bells, and a rabbit called Sandy. Dear Jingle Bells
+            did a disappearing act one day – bending apart the bars of his cage and escaping. The suspicion was that he was
+            aided by either my eldest brother or my Dad, but neither owned up and this will remain a mystery for all time now.
+            Sandy, on the other hand, I believe, made his own escape. He was housed in a run in the garden. He had all the
+            creature comforts that a bunny could want but one day (or night) he dug under the wire of the run and escaped too.
+            I can remember searching the streets for Sandy and searching every cupboard and nook and cranny in the house for
+            Jingle Bells, but we never saw either of them again. So I can say that my first foray into keeping animals wasn’t
+            all that successful.
+        </p>
+      </div>
+      <Lassie />
+    </>
   )
 }
 
