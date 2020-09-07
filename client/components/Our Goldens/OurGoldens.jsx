2215104f474d584bd326158f1920144bdeb3b03c
petemcnie <petemcnie0@gmail.com>
2020-09-08 10:14:19 +1200

    Our Goldens component created

---
+++ client/components/Our Goldens/OurGoldens.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import NavbarComp from '../NavbarComp'
+
+function OurGoldens () {
+  return (
+    <>
+      <NavbarComp />
+      <div className='belowNavbar'>
+        <h1>Our Goldens</h1>
+      </div>
+    </>
+  )
+}
+
+export default OurGoldens
