cca40df44a95fe9b68162e258f8deedb1056cd6e
petemcnie <petemcnie0@gmail.com>
2020-09-11 10:23:05 +1200

    Sherry component content added

--- client/components/Our Goldens/Sherry.jsx
+++ client/components/Our Goldens/Sherry.jsx
@@ -13,50 +13,83 @@ function Sherry () {
         <h2>Sherry</h2>
       </div>
       <div>
-        <h3></h3>
-        <p>Sire: Braeview the Kingmaker</p>
-        <p>Dam: Honey of Frandale</p>
-        <p>10 January 2002 - 14 September 2016</p>
-        <p></p>
+        <h3>CH Golden Light at Salhouse CDX (Sherry)</h3>
+        <p>Sire: CH Sandlewood Gamble-with-Gold</p>
+        <p>Dam: CH Salhouse Sunchaser</p>
+        <p>Whelped: 7 April 2010</p>
+        <p>Hips: 8:8 Elbows: 1(a):B  Eyes: Clear 27/5/2014</p>
         <div>
           <Image src="images/sherry1.jpg" fluid />
         </div>
       </div>
       <div>
-        <h4>Achievements:</h4>
         <p>
-            Sherry was my first golden and is a beautiful pet. I have dabbled in agility and a tiny bit of obedience with her but
-            really she is mainly a pet who lives with us and enjoys herself, mostly. She does have one or two phobias, e.g. she
-            doesn&apos;t like thunder and tends to be a bit claustrophobic. But other than that, she is a lovely pet. She is very gentle
-            and light on her feet and loves nothing more than to have a tug of war with whoever might be around to play, be it
-            human or dog!
+          Sherry is the newest addition to our furry family. She is a very sweet puppy who was chosen for her friendly nature,
+          waggy tail and, hopefully, suitability to be a show dog. She has good bone structure at this stage. She has upset the
+          rest of the dogs in our family temporarily, but that will settle down once she has grown up a bit and especially when
+          she loses her needle-like baby teeth and grows some proper teeth. She is quick to learn and at 10 weeks of age can sit,
+          down, wait, stand, come to command. However, she can&apos;t let go of her mother&apos;s leg when called so there&apos;s still some
+          training to do there.
         </p>
         <p>
-            About three years ago she was diagnosed with cataracts forming in her eyes. Then a year ago we discovered that she also
-            had retinal degeneration. She is 8 years old now and is partially blind. We may decide to have the cataracts operated
-            on some time this year, but there is no cure for the retinal degeneration which will eventually make her totally blind.
-            So it will be interesting to see how she gets on over the next couple of years. At the moment, even though her eyesight
-            is generally poor, when we go for walks in the bush, even at night, she is far better finding her way along the path
-            than I am. I who have to have a torch and fall off the path if I am not careful. Dogs obviously work on other senses
-            than eyesight to guide them. We will have to help her though and it will be a challenge but one that we will face together.
-            Sherry was spayed when she was young so we will not be passing this particular problem on to others.
+          Sherry is now 17 weeks old and enjoying life to the full. She has learnt to swim and loves running around on the beach.
+          She is a real retriever as she always brings us something in her mouth when she comes to see us - either a slipper, sock,
+          toy, anything. Yesterday I dropped a birthday card on the floor and she picked it up and gave it to me! Sherry will be going to her first show on 8 August so it will be interesting to see how she gets on there.
         </p>
         <p>
-            To find out more about how Sherry came into our lives, go to the My Life with Animals page.
+          Sherry has achieved some successes as Baby Puppy in Breed and in the middle of August at 4 and a half months old, she went
+          to the Auckland Golden Retriever shows and won Baby Puppy in Show at both shows. Critique for one of the shows was: Good
+          size young lady sweetest of heads, good pigment, kind eye, well angulated at front and rear, dead level topline, has good
+          overall balance, moved out well for one so young, shows good promise.
         </p>
         <p>
-            Sherry finally passed away on 14 September 2016. That was a very sad day. No, I can&apos;t just say that... that was a
-            devastating day. We did it all in the best way we could for her and for that I am forever thankful. However, it was
-            still too soon. I wanted to hold her forever and keep her, but for her sake it was kindest to let her go. She took a
-            piece of our hearts with her and we will never forget her. Our lasting message for our beautiful Sherry was &ldquo;Thank you
-            for sharing your life with us.&ldquo; I miss you so very much my beautiful girl! xx
+          Sherry has now moved on to Puppy class and has had one or two successes at the end of 2010.  In Hastings in early December
+          she won two Puppy of Breeds.  She is developing really nicely. (See 8 month photo below.)
+        </p>
+        <p>
+          Sherry has also developed her own wonderful personality and one of her most endearing traits is that she always  has
+          something in her mouth when she comes to see you.  If she is rushing up to greet you and doesn&apos;t have anything in her
+          mouth she stops dead, and you can see the thought processes - nothing in mouth, must get something to bring as a gift!
+          So sweet.  She has also taken up putting her nose in everything, although she denies it of course (see photos below).
+        </p>
+        <p>
+        Sherry has now got a wonderful trick - when I come in from outside the house she immediately runs off to find my slippers
+        and brings them to me one at a time. You can see her thinking when I come in that she has got to find the slippers, without
+        me even telling her. Sometimes when I come in from downstairs she is already waiting at the top of the stairs with one of
+        them - she just can&apos;t wait to give them to me.
+        </p>
+        <p>
+          September 2013 - Sherry has been competing in obedience competitions for about a year and a half and has just won out of
+          Special Beginners. I&apos;m so proud of her. Photo and news on the news page.
+        </p>
+        <p>
+          March 2014 - Two weeks after her mum, Shadow, became a Champion at Wanganui, we set off for four more shows at Hastings.
+          The weather was lovely, the company was great and the mood seemed good. On Saturday morning we went into the show ring
+          and Sherry got into the line up. She was running really nicely and the judge looked and looked and all of a sudden pointed
+          to Sherry for the challenge! It took a moment to dawn on me... we have finally made another Champion... yeah. What excitement
+          that was. We were on Cloud 9. We had a lovely boozy shout at lunch time to celebrate our success with friends who have
+          supported us throughout the years we have been showing. It was a really nice time and something I won&apos;t forget. To top
+          the weekend off, on the Sunday Shadow took the challenge in the morning show and Sherry took another challenge in the
+          afternoon show - so what a great effort that was from both the girls. We didn&apos;t get home till after 9pm that day!
+        </p>
+        <p>
+          May 2014 - Well, what a year this is becoming for Sherry. She entered her first CD for obedience in Wanganui on 3 May
+          2014 and passed with flying colours gaining her CDX obedience title. We love Wanganui!
+        </p>
+        <p>
+          February 2015 - Sherry and I finally got it together, and after nearly a year in Novice obedience we won one! We only
+          lost .5 out of 100 and heat 39 other dogs! Go my sweet girl!
+        </p>
+        <p>
+            March 2015 - We did it again a couple of weeks later - won another Novice competition and so now Sherry is official a
+            Test A dog having won the two Novice tests to qualify for Test A.
         </p>
       </div>
       <div>
         <Table bordered size='sm' responsive="sm">
           <thead>
             <tr>
-              <th colSpan='3'>Pedigree of Sanastasia Sth&apos;n Star Chara</th>
+              <th colSpan='3'>Pedigree of CH Golden Light at Salhouse CDX (Sherry)</th>
             </tr>
           </thead>
           <tbody>
@@ -66,34 +99,34 @@ function Sherry () {
               <td>Great Grandparents</td>
             </tr>
             <tr>
-              <td rowSpan='4'>Sire: CH Seacove Southern Cross</td>
-              <td rowSpan='2'>CH Glentreve Frozen-Asset</td>
-              <td>Eng. CH Paudell Easter Plantagenet at Kerrien (UK)</td>
+              <td rowSpan='4'>Sire: CH Sandlewood Gamble-with-Gold</td>
+              <td rowSpan='2'>Goldfleece Go for Gold at Sandlewood (Imp Aust)</td>
+              <td>Aust CH Stanroph Shared Assets (Imp UK)</td>
             </tr>
             <tr>
-              <td>CH McIlroy Interlude</td>
+              <td>Dobro Free as a Bird</td>
             </tr>
             <tr>
-              <td rowSpan='2'>Seacove Bireme</td>
-              <td>GR & Aust. CH Caldicot Swift</td>
+              <td rowSpan='2'>CH Sandlewood Charlies Angel</td>
+              <td>CH McIlroy Millennium Choir Boy at Sandlewood</td>
             </tr>
             <tr>
-              <td>Goldriver Amber-Sunrise QC CDX</td>
+              <td>CH Sandlewood Cinderella</td>
             </tr>
             <tr>
-              <td rowSpan='4'>Dam: Sanastasia Treasured Teah CDX</td>
-              <td rowSpan='2'>OB GR CH Speyside Fidgety Feet CDX</td>
-              <td>GR CH Graveleigh Soaring High QC CDX</td>
+              <td rowSpan='4'>Dam: CH Salhouse Sunchaser</td>
+              <td rowSpan='2'>NZ CH Santamaria The Patriot (Imp Aust)</td>
+              <td>Aust CH Strathraer Nordic Star</td>
             </tr>
             <tr>
-              <td>Speyside Shades of Gold CDX</td>
+              <td>Aust CH Santamaria Anna Leise</td>
             </tr>
             <tr>
-              <td rowSpan='2'>Sanastasia Special Lady CDX</td>
-              <td>Sanastasia Gold Falkor</td>
+              <td rowSpan='2'>Sanastasia Sth&apos;n Star Chara</td>
+              <td>CH Seacove Southern Cross</td>
             </tr>
             <tr>
-              <td>Speyside Silver Lining CDX</td>
+              <td>Sanastasia Treasured Teah CDX</td>
             </tr>
           </tbody>
         </Table>
@@ -102,28 +135,45 @@ function Sherry () {
       <div>
         <p>Photos:</p>
         <Image src="images/sherry2.jpg" fluid />
-        <p>Puppy Sherry used to be a doorstop!</p>
+        <p>Sherry winning her second Novice test at Kapiti March 2015</p>
         <Image src="images/sherry3.jpg" fluid />
-        <p></p>
+        <p>Sherry winning her first Novice test at Levin Feb 2015</p>
         <Image src="images/sherry4.jpg" fluid />
         <Image src="images/sherry5.jpg" fluid />
-        <p></p>
+        <p>Cool dude Sherry (left). Put him down Sherry (right).</p>
         <Image src="images/sherry6.jpg" fluid />
-        <p></p>
         <Image src="images/sherry7.jpg" fluid />
-        <p></p>
+        <p>I didn&apos;t put my nose in the paint pot (left) or dig in the garden (right), honest!</p>
         <Image src="images/sherry8.jpg" fluid />
-        <p></p>
         <Image src="images/sherry9.jpg" fluid />
-        <p></p>
         <Image src="images/sherry10.jpg" fluid />
-        <p></p>
+        <p>Sherry with various items in her mouth - apple (left); slipper (centre); toy (right).</p>
         <Image src="images/sherry11.jpg" fluid />
-        <p></p>
+        <p>Sherry at 10 months</p>
         <Image src="images/sherry12.jpg" fluid />
-        <p></p>
+        <p>Sherry at 8 months</p>
         <Image src="images/sherry13.jpg" fluid />
-        <p></p>
+        <p>Sherry at 6 months</p>
+        <Image src="images/sherry14.jpg" fluid />
+        <p>Sherry with her winnings from the Golden Retriever Club Show held in Auckland in August 2010.</p>
+        <Image src="images/sherry15.jpg" fluid />
+        <p>Sherry sharing her mum&apos;s bed!</p>
+        <Image src="images/sherry16.jpg" fluid />
+        <p>Sherry enjoys chasing Kula at the beach (15 weeks).</p>
+        <Image src="images/sherry17.jpg" fluid />
+        <p>Jeff and Sherry at the top of Sugarloaf Rock (14 weeks)!</p>
+        <Image src="images/sherry18.jpg" fluid />
+        <p>Sherry at the beach at 12 weeks.</p>
+        <Image src="images/sherry19.jpg" fluid />
+        <p>Sherry with a toy at 9 weeks old.</p>
+        <Image src="images/sherry20.jpg" fluid />
+        <p>Sherry and Shadow cuddle up.</p>
+        <Image src="images/sherry21.jpg" fluid />
+        <p>Sherry enjoys a kip in the sun.</p>
+        <Image src="images/sherry22.jpg" fluid />
+        <p>Sherry at 12 days old, playing with a ball already!</p>
+        <Image src="images/sherry23.jpg" fluid />
+        <p>Sherry at 1 day old</p>
       </div>
     </>
   )
