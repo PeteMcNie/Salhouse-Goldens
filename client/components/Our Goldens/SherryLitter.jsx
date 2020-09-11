a7205ae7c88cf184030f0ac7ec197732f375b864
petemcnie <petemcnie0@gmail.com>
2020-09-11 14:19:03 +1200

    Litter template copied for other dogs

---
+++ client/components/Our Goldens/SherryLitter.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+import Image from 'react-bootstrap/Image'
+
+function KulasLitter () {
+  return (
+    <>
+      <div>
+        <h2>Kula&apos;s Litter 2006</h2>
+      </div>
+      <div>
+        <Image src="images/kl1.jpg" fluid />
+        <p>On 21 February 2006, Kula (pictured left top) had a litter of 7 beautiful puppies, 5 boys and 2 girls.</p>
+        <Image src="images/kl2.jpg" fluid />
+        <p>The father of these puppies is Jordan (NZ CH Santamaria The Patriot) (pictured left bottom).</p>
+      </div>
+      <hr />
+      <div>
+        <p>Photos:</p>
+        <Image src="images/kl2.jpg" fluid />
+        <p></p>
+        <Image src="images/kl2.jpg" fluid />
+        <p></p>
+        <Image src="images/kl2.jpg" fluid />
+        <p></p>
+        <Image src="images/kl2.jpg" fluid />
+        <p></p>
+        <Image src="images/kl2.jpg" fluid />
+        <p></p>
+        <Image src="images/kl2.jpg" fluid />
+        <p></p>
+        <Image src="images/kl2.jpg" fluid />
+        <p></p>
+        <Image src="images/kl2.jpg" fluid />
+        <p></p>
+        <Image src="images/kl2.jpg" fluid />
+        <p></p>
+        <Image src="images/kl2.jpg" fluid />
+        <p></p>
+      </div>
+    </>
+
+  )
+}
+
+export default KulasLitter
