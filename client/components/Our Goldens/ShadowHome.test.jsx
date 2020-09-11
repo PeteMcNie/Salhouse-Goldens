0bdf5bed0dc9464599e0cb58fdbb4183c8967a5c
petemcnie <petemcnie0@gmail.com>
2020-09-11 14:54:07 +1200

    Shadow test updated

--- client/components/Our Goldens/ShadowHome.test.jsx
+++ client/components/Our Goldens/ShadowHome.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 
-import Shadow from './Shadow'
+import Shadow from './ShadowHome'
 
 test('Shadown component matches snapshot', () => {
   const { container } = render(<Shadow />)
