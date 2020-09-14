3f3bc838743e723e0fb378e8440162aa940be88d
petemcnie <petemcnie0@gmail.com>
2020-09-15 10:21:50 +1200

    Basic Grooming tips component added

--- client/components/GYG/GYG.jsx
+++ client/components/GYG/GYG.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 import Navbar from '../NavbarComp'
 import GYGStart from './GYGStart'
+import BasicGroomingTips from './BasicGroomingTips'
 
 function GYG () {
   return (
     <>
       <Navbar />
       <GYGStart />
+      <BasicGroomingTips />
     </>
   )
 }
