425e2d65aa570da2d65eed6ef322067c9452c3d0
petemcnie <petemcnie0@gmail.com>
2020-09-19 11:53:42 +1200

    Phoenix test removed, Phoenix splider styled

--- client/components/Our Goldens/Phoenix.test.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { render } from '@testing-library/react'
-
-import Phoenix from './Phoenix'
-
-test('Phoenix component matches snapshot', () => {
-  const { container } = render(<Phoenix />)
-  expect(container).toMatchSnapshot()
-})
