3bec56b63d9df5ea65b702ed876f83165085ad8d
petemcnie <petemcnie0@gmail.com>
2020-09-30 16:47:46 +1300

    Added margin to bottom image on home page

--- client/components/Home/HomeFooter.jsx
+++ client/components/Home/HomeFooter.jsx
@@ -22,7 +22,7 @@ function HomeFooter () {
               If you want to use them under different terms, please contact me at
               <a href='mailto:cmcnie@xtra.co.nz?subject=Enquiry regarding using photos from your Webpage'> cmcnie@xtra.co.nz</a>
             </p>
-            <a href='https://creativecommons.org/licenses/by-nc-nd/3.0/nz/'><img src='images/nocc.png' /></a>
+            <a href='https://creativecommons.org/licenses/by-nc-nd/3.0/nz/'><img src='images/nocc.png' className='bottom' /></a>
           </Col>
         </Row>
       </Container>
