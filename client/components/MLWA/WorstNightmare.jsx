f03240b4d3e1a2f4514acd75eccb7d16fae64c27
petemcnie <petemcnie0@gmail.com>
2020-10-08 10:56:19 +1300

    MLWA styled to Worstnightmare

--- client/components/MLWA/WorstNightmare.jsx
+++ client/components/MLWA/WorstNightmare.jsx
@@ -1,108 +1,147 @@
 import React from 'react'
 
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 import Image from 'react-bootstrap/Image'
 
 function WorstNightmare () {
   return (
     <>
-      <div>
-        <h3>Worst nightmare</h3>
-        <p>
-            Then in December 2001 Pepper went off her food. She was the sort of dog that ate ravenously. It didn’t matter how
-            much food was in her bowl, it all went within about 10 seconds, and heaven help anyone who got near. I had tried
-            over the years to get her to let me stroke her while she was eating or to move the bowl. I did have some success
-            but would never have trusted her with the kids so guarded her while she ate – which didn’t take much time out of
-            my day due to the speed of this process! I can’t tell you how scary it is to have an animal that you love start to
-            get sick and for there to seem to be nothing you can do about it. For the previous two winters, Pepper had been
-            allowed to sleep inside every night as my conscience had been getting to me and I had started to think that there
-            was a better way to look after this dog at home. I also had her in the lounge during the day when the kids weren’t
-            around and the weather was bad.
-        </p>
-        <p>
-            None of this prepared me for what was about to happen. I took Pepper to the vet and he did the usual process ...
-            antibiotics first ... when that didn’t work he took blood tests ... when those came back with slight abnormalities
-            he did another blood test. Pepper and I would walk down the vet and she would stop at the door to the vet in the
-            morning and I remember thinking that this beautiful creature has no idea that this place is possibly going to be
-            where she ends her life one day. All the days that these tests were taking, Pepper wasn’t eating. I tried everything
-            that I could think of. She certainly wasn’t eating her dried dog food. For a while she nibbled a bit of wet food. I
-            would offer her some of our tea, I would buy things for her to try, but mostly to no avail. I was watching this
-            lovely, active but slim dog become even slimmer.
-        </p>
-        <Image src="images/peppersplashes.jpg" fluid />
-        <p>
-            We went through Christmas that year with a bit of a struggle. We usually bought Pepper some doggie choc drops for
-            Christmas, but this year she opened her present but wasn’t interested in eating. I was worried sick about her. The
-            final blood test that she had showed the possibility of something wrong with the liver. One day the vet talked to
-            me about the fact that if Pepper wasn’t eating, she would probably die and that while she seemed ok she could go on
-            but there could be a time when it would be best to put her to sleep to speed up the inevitable. I talked to him
-            about what else could be done. The fact was that at that time, Pepper was still enjoying walks and seemed quite
-            energetic. I still to this day don’t know how she managed that with the tiny amount of food that she had had for a
-            couple of weeks. The vet said that he could do a biopsy on the liver to see what was wrong with the hope that he
-            would be able to find a cure. We were lucky at this time that we were in a position to try anything to save Pepper.
-            So we agreed that she would go in for the biopsy on Monday.
-        </p>
-        <Image src="images/pepperChristmas.jpg" fluid />
-        <p>
-            That weekend Rob and I took Pepper to her favourite walks. We walked down the river and she gambolled along in front
-            of us. Probably not quite as racey as normal, but still enjoying it. We sat down on the grass and played with her
-            and just sat and enjoyed the togetherness.
-        </p>
-        <div>
-          <Image src="images/pepperBeach.jpg" fluid />
-          <p>Our last beach walk with Pepper - still as keen as ever!</p>
-        </div>
-        <p>
-            But of course the good time had to end and Monday morning came. Once again, Pepper walked happily to the vet and
-            waited at the door to be let in. I said goodbye and left her there for the day. The vet called later to say the
-            biopsy had gone ok and was to be sent off and I could pick her up after 4pm. Well, who knows whether I would have
-            done this if I had known what was going to happen, or whether I would have just let Pepper get to the point where
-            we just put her to sleep without putting her through this operation. We can always have regrets in hindsight and
-            wish for time to be turned back to see if another route would prove to be better. However, I made my choice and this
-            is what happened to my beautiful Pepper....
-        </p>
-        <p>
-            Having not eaten for quite a while, the anaesthetic hit her pretty hard. She was so groggy all evening and couldn’t
-            really move. I remember coaxing her for a short walk up the road in the evening to see if she wanted to relieve
-            herself, but she didn’t. We literally only staggered two houses up the street and back. I thought she would enjoy
-            being out in the air.
-        </p>
-        <Image src="images/pepper1.jpg" fluid />
-        <p>
-            She had a bed right by my side of the bed so I could keep an eye on her. We had visitors staying that night – in the
-            lounge – so my life was a bit of an upheaval at that time. But I checked on Pepper every few moments it felt like.
-            It was the worst night of my life. I went to bed and Pepper, who was laying down would then struggle up and just
-            stand. I would stroke her head and try and get her to lay down again but she just wanted to stand. She must have
-            felt so awful, probably in a lot of pain, but such a quiet, docile animal here. This dog who loved nothing more than
-            to run and run, standing by my bed with her head down. I was awake all night with her, just stroking her head. The
-            longest night of my life. As soon as it was morning I got up to look at her. She was laying down at that stage and
-            I know I knew... you know... that time that you dread, that time that you know you have to be God. She was laying
-            on the blanket and the wound in her tummy was oozing slightly with blood. I had to get her to the vet. They don’t
-            answer the phone until 8am, but I knew that the vet was usually there at 7.30am. I told Rob I needed to take her
-            immediately. We carried her to the car and laid her in the back and I went to the vet. I had never done anything
-            like this before and my mind was in a spin with a huge cloud over it. I was so relieved that the vet was there and
-            he came to the car and looked at her. Her gums were totally yellow and he said that her liver had stopped working.
-            We carried her into the surgery, laid her on the table and there I stroked her beautiful head and talked to her
-            while she slipped from this world to a happier place.
-        </p>
-        <p>
-            I took her home and once our visitors had gone, we all said goodbye to Pepper and Rob buried her in the garden
-            along with all our other “friends”. That was a hard day. The boys and we all put something special into that grave
-            for Pepper to take with her – treats, ball, stick and a blanket to keep her warm. At least I got to say goodbye to
-            this dog and to tell her I loved her.
-        </p>
-        <div>
-          <Image src="images/pepperRiver.jpg" fluid />
-          <p>Pepper loved nothing more than to be playing at the river, soaking wet and happy!</p>
-        </div>
-        <p>
-            Pepper gave me something very special. She was my first dog and I made what I consider to be mistakes with her.
-            The main thing I feel I failed on was that I didn’t keep her in the house with us as part of the family. This was
-            on my conscience as time went on and was why for the last couple of years she had spent more time in the house.
-            However, I felt I needed to make amends.
-        </p>
-      </div>
-      <hr />
+      <Container>
+        <Row className='row'>
+          <Col lg={6} className='leftCol'>
+            <h2 className='bodyTitle'>Worst nightmare</h2>
+            <p>
+              Then in December 2001 Pepper went off her food. She was the sort of dog that ate ravenously. It didn’t matter how
+              much food was in her bowl, it all went within about 10 seconds, and heaven help anyone who got near. I had tried
+              over the years to get her to let me stroke her while she was eating or to move the bowl. I did have some success
+              but would never have trusted her with the kids so guarded her while she ate – which didn’t take much time out of
+              my day due to the speed of this process! I can’t tell you how scary it is to have an animal that you love start to
+              get sick and for there to seem to be nothing you can do about it. For the previous two winters, Pepper had been
+              allowed to sleep inside every night as my conscience had been getting to me and I had started to think that there
+              was a better way to look after this dog at home. I also had her in the lounge during the day when the kids weren’t
+              around and the weather was bad.
+            </p>
+            <p>
+              None of this prepared me for what was about to happen. I took Pepper to the vet and he did the usual process ...
+              antibiotics first ... when that didn’t work he took blood tests ... when those came back with slight abnormalities
+              he did another blood test. Pepper and I would walk down the vet and she would stop at the door to the vet in the
+              morning and I remember thinking that this beautiful creature has no idea that this place is possibly going to be
+              where she ends her life one day. All the days that these tests were taking, Pepper wasn’t eating. I tried everything
+              that I could think of. She certainly wasn’t eating her dried dog food. For a while she nibbled a bit of wet food. I
+              would offer her some of our tea, I would buy things for her to try, but mostly to no avail. I was watching this
+              lovely, active but slim dog become even slimmer.
+            </p>
+          </Col>
+          <Col lg={6}>
+            <div>
+              <Image src="images/peppersplashes.jpg" fluid />
+            </div>
+          </Col>
+        </Row>
+        <Row className='row'>
+          <Col lg={6} className='leftCol'>
+            <p>
+              We went through Christmas that year with a bit of a struggle. We usually bought Pepper some doggie choc drops for
+              Christmas, but this year she opened her present but wasn’t interested in eating. I was worried sick about her. The
+              final blood test that she had showed the possibility of something wrong with the liver. One day the vet talked to
+              me about the fact that if Pepper wasn’t eating, she would probably die and that while she seemed ok she could go on
+              but there could be a time when it would be best to put her to sleep to speed up the inevitable. I talked to him
+              about what else could be done. The fact was that at that time, Pepper was still enjoying walks and seemed quite
+              energetic. I still to this day don’t know how she managed that with the tiny amount of food that she had had for a
+              couple of weeks. The vet said that he could do a biopsy on the liver to see what was wrong with the hope that he
+              would be able to find a cure. We were lucky at this time that we were in a position to try anything to save Pepper.
+              So we agreed that she would go in for the biopsy on Monday.
+            </p>
+          </Col>
+          <Col lg={6}>
+            <div>
+              <Image src="images/pepperChristmas.jpg" fluid />
+            </div>
+          </Col>
+        </Row>
+        <Row className='row'>
+          <Col lg={6} className='leftCol'>
+            <p>
+              That weekend Rob and I took Pepper to her favourite walks. We walked down the river and she gambolled along in front
+              of us. Probably not quite as racey as normal, but still enjoying it. We sat down on the grass and played with her
+              and just sat and enjoyed the togetherness.
+            </p>
+            <p>
+              But of course the good time had to end and Monday morning came. Once again, Pepper walked happily to the vet and
+              waited at the door to be let in. I said goodbye and left her there for the day. The vet called later to say the
+              biopsy had gone ok and was to be sent off and I could pick her up after 4pm. Well, who knows whether I would have
+              done this if I had known what was going to happen, or whether I would have just let Pepper get to the point where
+              we just put her to sleep without putting her through this operation. We can always have regrets in hindsight and
+              wish for time to be turned back to see if another route would prove to be better. However, I made my choice and this
+              is what happened to my beautiful Pepper....
+            </p>
+          </Col>
+          <Col lg={6}>
+            <div>
+              <Image src="images/pepperBeach.jpg" fluid />
+              <p>Our last beach walk with Pepper - still as keen as ever!</p>
+            </div>
+          </Col>
+        </Row>
+        <Row className='row'>
+          <Col lg={6} className='leftCol'>
+            <p>
+              Having not eaten for quite a while, the anaesthetic hit her pretty hard. She was so groggy all evening and couldn’t
+              really move. I remember coaxing her for a short walk up the road in the evening to see if she wanted to relieve
+              herself, but she didn’t. We literally only staggered two houses up the street and back. I thought she would enjoy
+              being out in the air.
+            </p>
+            <p>
+              She had a bed right by my side of the bed so I could keep an eye on her. We had visitors staying that night – in the
+              lounge – so my life was a bit of an upheaval at that time. But I checked on Pepper every few moments it felt like.
+              It was the worst night of my life. I went to bed and Pepper, who was laying down would then struggle up and just
+              stand. I would stroke her head and try and get her to lay down again but she just wanted to stand. She must have
+              felt so awful, probably in a lot of pain, but such a quiet, docile animal here. This dog who loved nothing more than
+              to run and run, standing by my bed with her head down. I was awake all night with her, just stroking her head. The
+              longest night of my life. As soon as it was morning I got up to look at her. She was laying down at that stage and
+              I know I knew... you know... that time that you dread, that time that you know you have to be God. She was laying
+              on the blanket and the wound in her tummy was oozing slightly with blood. I had to get her to the vet. They don’t
+              answer the phone until 8am, but I knew that the vet was usually there at 7.30am. I told Rob I needed to take her
+              immediately. We carried her to the car and laid her in the back and I went to the vet. I had never done anything
+              like this before and my mind was in a spin with a huge cloud over it. I was so relieved that the vet was there and
+              he came to the car and looked at her. Her gums were totally yellow and he said that her liver had stopped working.
+              We carried her into the surgery, laid her on the table and there I stroked her beautiful head and talked to her
+              while she slipped from this world to a happier place.
+            </p>
+          </Col>
+          <Col lg={6}>
+            <div>
 
+              <Image src="images/pepper1.jpg" fluid />
+            </div>
+          </Col>
+        </Row>
+        <Row className='row'>
+          <Col lg={6} className='leftCol'>
+            <p>
+              I took her home and once our visitors had gone, we all said goodbye to Pepper and Rob buried her in the garden
+              along with all our other “friends”. That was a hard day. The boys and we all put something special into that grave
+              for Pepper to take with her – treats, ball, stick and a blanket to keep her warm. At least I got to say goodbye to
+              this dog and to tell her I loved her.
+            </p>
+            <p>
+              Pepper gave me something very special. She was my first dog and I made what I consider to be mistakes with her.
+              The main thing I feel I failed on was that I didn’t keep her in the house with us as part of the family. This was
+              on my conscience as time went on and was why for the last couple of years she had spent more time in the house.
+              However, I felt I needed to make amends.
+            </p>
+          </Col>
+          <Col lg={6}>
+            <div>
+              <Image src="images/pepperRiver.jpg" fluid />
+              <p>Pepper loved nothing more than to be playing at the river, soaking wet and happy!</p>
+            </div>
+          </Col>
+        </Row>
+        <hr className='sectionSeparator' />
+      </Container>
     </>
   )
 }
