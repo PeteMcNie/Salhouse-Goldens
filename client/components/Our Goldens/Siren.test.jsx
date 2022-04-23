84f0a0bf4735f75eb6cb1296278a6a996c1e284b
petemcnie <petemcnie0@gmail.com>
2022-04-23 14:23:32 +1200

    Remove test file

--- client/components/Our Goldens/Siren.test.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { render } from '@testing-library/react'
-
-import Siren from './Siren'
-
-xtest('Siren component matches snapshot', () => {
-  const { container } = render(<Siren />)
-  expect(container).toMatchSnapshot()
-})
